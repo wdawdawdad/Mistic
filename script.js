d9b36ebc08642002b1b9e085254bc6f35405a863
wdawdawdad <esdsef2@gmail.com>
2025-01-31 15:01:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createSnowflake() {
     const snowflake = document.createElement("div");
     snowflake.classList.add("snowflake");
-    snowflake.innerHTML = "❄";
+    snowflake.innerHTML = "💜";
     snowflake.style.left = Math.random() * window.innerWidth + "px";
     snowflake.style.animationDuration = (Math.random() * 3 + 2) + "s"; 
     snowflake.style.fontSize = (Math.random() * 10 + 10) + "px"; 
